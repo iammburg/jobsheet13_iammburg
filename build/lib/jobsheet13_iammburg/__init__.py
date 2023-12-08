e4daa54aaa87ece78ba0e11c2ee35d145bec87e7
iammburg <ilhamprabowo009@gmail.com>
2023-12-08 18:43:50 +0700

    create package

---
+++ build/lib/jobsheet13_iammburg/__init__.py
@@ -0,0 +1,9 @@
+"""
+pyexample.
+
+An example python library.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Ilham Andaru'
+__credits__ = 'Politeknik Negeri Semarang'
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jobsheet13_iammburg',
+    version='0.1.0',
+    author='Your Name: Udin',
+    author_email='ilhamprabowo009@gmai.com',
+    description='Sebuah paket tebak angka dan konversi berat',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
